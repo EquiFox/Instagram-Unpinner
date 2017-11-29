bae9c953c6b9a36096064ef3b5d1c1251f11d0c7
EquiFox <contact@reversing.ninja>
2017-11-29 12:16:38 -0500

    Updated setup file

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
     description = 'Utility used to patch Instagram APK in order to disable Certificate Pinning protection.',
     author = 'EquiFox',
     author_email = 'contact@reversing.ninja',
-    url = 'github package source url',
-    download_url = 'download link you saved',
+    url = 'https://github.com/EquiFox/Instagram-Unpinner',
+    download_url = 'https://github.com/EquiFox/Instagram-Unpinner/archive/v2.0.tar.gz',
     keywords = ['instagram', 'certificate', 'pinning'],
     classifiers = [],
 )
